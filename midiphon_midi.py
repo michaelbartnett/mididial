097a847086300383ac7fe69e05602bcc1ed9fcd4
Michael Bartnett <michael.bartnett@gmail.com>
2011-10-02 09:46:35 -0400

    Adding midi file writing + bitly

--- midiphon_midi.py
+++ midiphon_midi.py
@@ -1,8 +1,26 @@
 import random
 import rtmidi
 import threading
+from midiutil.MidiFile import MIDIFile
+import time
+import uuid
+import ftplib
 
 random.seed()
+
+def uploadMidiFile(filename):
+	ftp = ftplib.FTP('midiphon.bartnett.com', 'midiphon_uploader', 'grantophone')
+	with mf = open(filename):
+		ftp.storbinary('STOR ' + filename, mf)
+	
+	link = getBitlyLink()
+	sendBitlyLink(link)
+
+def getBitlyLink(url):
+	pass
+
+def sendBitlyLink(link):
+	pass
 
 class PhoneMusician(object):
 	def __init__(self, phoneNumber, midiChannel):
@@ -30,10 +48,10 @@ class PhoneMusician(object):
 	
 	def getNoteOnMessage(self, digit):
 		if digit == '*':
-			self.octaveUp(1)
+			self.octaveDown(1)
 			return None
 		elif digit == '#':
-			self.octaveDown(1)
+			self.octaveUp(1)
 			return None
 		return rtmidi.MidiMessage.noteOn(self.midiChannel, self.noteMap[digit] + self.octaveModifier * 12 + self.semitoneModifier, 127)
 
@@ -66,6 +84,8 @@ class MidiManager(object):
 		self.min.openVirtualPort(MidiManager.MidiPortName)
 		self.mout.openVirtualPort(MidiManager.MidiPortName)
 		self.midiPort = -1
+		self.startedPlaying = False
+		self.initialTime = time.clock()
 		for i in range(0, self.mout.getPortCount()):
 			if self.mout.getPortName(i) == MidiManager.MidiPortName:
 				self.midiPort = i
@@ -77,6 +97,11 @@ class MidiManager(object):
 
 
 	def addPlayer(self, phone):
+		if not self.startedPlaying:
+			self.startedPlaying = True
+			self.midiFile = MIDIFile(10)
+			self.midiFile.addTempo(0, 0, 60)
+
 		if self.midiChannels.has_key(phone):
 			return True
 		
@@ -95,15 +120,23 @@ class MidiManager(object):
 			print "Removing player {0} with midi channel {1}".format(phone, channelNumber)
 			self.availableChannels.append(channelNumber)
 			self.oldPlayers[phone] = player
-
+			if len(self.midiChannels) < 1:
+				fn = str(uuid.uuid1())
+				with f = open(fn, 'wb'):
+					self.midiFile.writeFile(f)
+				
 	
 	def playNote(self, phone, digit):
 		if self.midiChannels.has_key(phone):
-			msg = self.midiChannels[phone].getNoteOnMessage(digit)
+			player = self.midiChannels[phone]
+			msg = player.getNoteOnMessage(digit)
 			if msg:
+				noteLength = 1.5
+				elapsedTime = time.clock() - self.initialTime
+				self.midiFile.addNote(player.getMidiChannel(), player.getMidiChannel(), msg.getNoteNumber(), elapsedTime, noteLength, 127)
 				print "Sending midi message: channel={0} note={1} vel={2}".format(msg.getChannel(), msg.getNoteNumber(), msg.getVelocity())
 				self.mout.sendMessage(msg)
-				t = threading.Timer(3.0, self.stopNote, [phone, digit])
+				t = threading.Timer(noteLength, self.stopNote, [phone, digit])
 				t.start()
 
 			return True
