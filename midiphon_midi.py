2d004ea0a8e99d79d84e569746b3f403e3143ed3
Michael Bartnett <michael.bartnett@gmail.com>
2011-10-05 02:21:51 -0400

    Added optparse

--- midiphon_midi.py
+++ midiphon_midi.py
@@ -17,182 +17,190 @@ twilio_app_sid = os.environ["TWILIO_APP_SID"]
 bitly_api_token = os.environ["BITLY_API_TOKEN"]
 
 class PhoneMusician(object):
-	def __init__(self, phoneNumber, midiChannel):
-		self.midiChannel = midiChannel
-		self.phoneNumber = phoneNumber
-		self.octaveModifier = 0
-		self.semitoneModifier = 0
-		self.noteMap = {
-			'1' : 60,
-			'2' : 62,
-			'3' : 64,
-			'4' : 65,
-			'5' : 67,
-			'6' : 69,
-			'7' : 71,
-			'8' : 72,
-			'9' : 74,
-			'0' : 76,
-			# '*' : 77,
-			# '#' : 79,
-		}
-	
-	def getMidiChannel(self):
-		return self.midiChannel
-	
-	def getNoteOnMessage(self, digit):
-		if digit == '*':
-			self.octaveDown(1)
-			return None
-		elif digit == '#':
-			self.octaveUp(1)
-			return None
-		return rtmidi.MidiMessage.noteOn(self.midiChannel, self.noteMap[digit] + self.octaveModifier * 12 + self.semitoneModifier, 127)
-	
-	def getPhoneNumber():
-		return self.phoneNumber
+    def __init__(self, phoneNumber, midiChannel):
+        self.midiChannel = midiChannel
+        self.phoneNumber = phoneNumber
+        self.octaveModifier = 0
+        self.semitoneModifier = 0
+        self.noteMap = {
+            '1' : 60,
+            '2' : 62,
+            '3' : 64,
+            '4' : 65,
+            '5' : 67,
+            '6' : 69,
+            '7' : 71,
+            '8' : 72,
+            '9' : 74,
+            '0' : 76,
+            # '*' : 77,
+            # '#' : 79,
+        }
+    
+    def getMidiChannel(self):
+        return self.midiChannel
+    
+    def getNoteOnMessage(self, digit):
+        if digit == '*':
+            self.octaveDown(1)
+            return None
+        elif digit == '#':
+            self.octaveUp(1)
+            return None
+        return rtmidi.MidiMessage.noteOn(self.midiChannel, self.noteMap[digit] + self.octaveModifier * 12 + self.semitoneModifier, 127)
+    
+    def getPhoneNumber():
+        return self.phoneNumber
 
-	def getNoteOffMessage(self, digit):
-		if digit == '*' or digit == '#':
-			return None
-		return rtmidi.MidiMessage.noteOff(self.midiChannel, self.noteMap[digit] + self.octaveModifier * 12 + self.semitoneModifier)
+    def getNoteOffMessage(self, digit):
+        if digit == '*' or digit == '#':
+            return None
+        return rtmidi.MidiMessage.noteOff(self.midiChannel, self.noteMap[digit] + self.octaveModifier * 12 + self.semitoneModifier)
 
-	def octaveUp(self, amount):
-		self.octaveModifier += amount
-	
-	def octaveDown(self, amount):
-		self.octaveModifier -= amount
-	
-	def semitoneUp(self, amount):
-		self.semitoneModifier += amount
+    def octaveUp(self, amount):
+        self.octaveModifier += amount
+    
+    def octaveDown(self, amount):
+        self.octaveModifier -= amount
+    
+    def semitoneUp(self, amount):
+        self.semitoneModifier += amount
 
-	def semitoneDown(self, amount):
-		self.semitoneModifier -= amount
-			
+    def semitoneDown(self, amount):
+        self.semitoneModifier -= amount
+            
 
 class MidiManager(object):
-	MidiPortName = "MIDIPHON"
+    MidiPortName = "MIDIPHON"
 
 
-	def uploadMidiFile(self, filename):
-		print "Uploading midi file"
-		ftp = ftplib.FTP('midiphon.bartnett.com', 'midiphon_uploader', 'grantophone')
-		mf = open(filename)
-		ftp.storbinary('STOR midiphon.bartnett.com/' + filename, mf)
-		mf.close()
-		print "Done storing midi file"
-		
-		link = self.getBitlyLink('http://midiphon.bartnett.com/' + str(filename))
-		print 'GOT LINK: ' + link
-		self.sendBitlyLink(link)
-		print "SMS Sent!"
+    def uploadMidiFile(self, filename):
+        print "Uploading midi file"
+        ftp = ftplib.FTP('midiphon.bartnett.com', 'midiphon_uploader', 'grantophone')
+        mf = open(filename)
+        ftp.storbinary('STOR midiphon.bartnett.com/' + filename, mf)
+        mf.close()
+        print "Done storing midi file"
+        
+        link = self.getBitlyLink('http://midiphon.bartnett.com/' + str(filename))
+        print 'GOT LINK: ' + link
+        self.sendBitlyLink(link)
+        print "SMS Sent!"
 
-	def getBitlyLink(self, url):
-		global bitly_api_token
-		print "Opening bitly connection"
-		connect = Connection("midiphon", bitly_api_token)
-		print "Shortening URL"
-		short = connect.shorten(url)
-		print "Done shortening URL"
-		return short['url']
+    def getBitlyLink(self, url):
+        global bitly_api_token
+        print "Opening bitly connection"
+        connect = Connection("midiphon", bitly_api_token)
+        print "Shortening URL"
+        short = connect.shorten(url)
+        print "Done shortening URL"
+        return short['url']
 
-	def sendBitlyLink(self, link):
-		for p in self.oldPlayers:
-			self.client.sms.messages.create(to=p, from_="13866434928", body=("Listen to your performance at " + link))
+    def sendBitlyLink(self, link):
+        for p in self.oldPlayers:
+            self.twilio_client.sms.messages.create(to=p, from_="13866434928", body=("Listen to your performance at " + link))
 
-		self.oldPlayers.clear()
-		self.startedPlaying = False
-
-
-	def __init__(self):
-		global twilio_api_token
-		global twilio_app_sid
-
-		self.midiChannels = {}
-		self.oldPlayers = {}
-		self.availableChannels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]#, 11, 12, 13, 14 ,15, 16]
-		self.mout = rtmidi.RtMidiOut()
-		self.min = rtmidi.RtMidiIn()
-		self.min.openVirtualPort(MidiManager.MidiPortName)
-		self.mout.openVirtualPort(MidiManager.MidiPortName)
-		self.midiPort = -1
-		self.startedPlaying = False
-		self.initialTime = time.clock()
-		# self.twilioClient = TwilioClient()
-		for i in range(0, self.mout.getPortCount()):
-			if self.mout.getPortName(i) == MidiManager.MidiPortName:
-				self.midiPort = i
-				break
-		
-		if self.midiPort == -1:
-			raise Exception('Epic failure of finding midi channel I just opened.')
-
-		self.client = TwilioRestClient(account=twilio_app_sid, token=twilio_api_token)
-
-		
+        self.oldPlayers.clear()
+        self.startedPlaying = False
 
 
-	def addPlayer(self, phone):
-		if not self.startedPlaying:
-			self.startedPlaying = True
-			self.midiFile = MIDIFile(10)
-			for i in range(0, 10):
-				self.midiFile.addTrackName(i, 0, str(i+1))
-				self.midiFile.addTempo(i, 0, 60)
+    def __init__(self, numChannels):
+        global twilio_api_token
+        global twilio_app_sid
 
-		if self.midiChannels.has_key(phone):
-			return True
-		
-		
-		channelCount = len(self.availableChannels)
-		if channelCount > 0:
-			self.midiChannels[phone] = PhoneMusician(phone, self.availableChannels.pop(random.randint(1, channelCount) - 1))
-			print "Adding player#" + phone
-			return True
-		
-		return False
-	
-	def removePlayer(self, phone):
-		if self.midiChannels.has_key(phone):
-			player = self.midiChannels.pop(phone)
-			channelNumber = player.midiChannel
-			print "Removing player {0} with midi channel {1}".format(phone, channelNumber)
-			self.availableChannels.append(channelNumber)
-			self.oldPlayers[phone] = player
-			if len(self.midiChannels) < 1:
-				fn = str(uuid.uuid1()) + '.mid'
-				f = open(fn, 'wb')
-				self.midiFile.writeFile(f)
-				f.close()
-				self.uploadMidiFile(fn)
-				
-	
-	def playNote(self, phone, digit):
-		if self.midiChannels.has_key(phone):
-			player = self.midiChannels[phone]
-			msg = player.getNoteOnMessage(digit)
-			if msg:
-				noteLength = 1.5
-				elapsedTime = (time.clock() - self.initialTime) * 60.0
-				self.midiFile.addNote(player.getMidiChannel() - 1, player.getMidiChannel(), msg.getNoteNumber(), elapsedTime, noteLength, 127)
-				print "Sending midi message: channel={0} note={1} vel={2}".format(msg.getChannel(), msg.getNoteNumber(), msg.getVelocity())
-				self.mout.sendMessage(msg)
-				t = threading.Timer(noteLength, self.stopNote, [phone, digit])
-				t.start()
+        self.midiChannels = {}
+        self.oldPlayers = {}
+        self.availableChannels = range(1, numChannels + 1)
+        self.mout = rtmidi.RtMidiOut()
+        self.min = rtmidi.RtMidiIn()
+        self.min.openVirtualPort(MidiManager.MidiPortName)
+        self.mout.openVirtualPort(MidiManager.MidiPortName)
+        self.midiPort = -1
+        self.startedPlaying = False
+        self.initialTime = time.clock()
+        for i in range(0, self.mout.getPortCount()):
+            if self.mout.getPortName(i) == MidiManager.MidiPortName:
+                self.midiPort = i
+                break
+        
+        if self.midiPort == -1:
+            raise Exception('Epic failure of finding midi channel I just opened.')
 
-			return True
+        self.twilio_client = TwilioRestClient(account=twilio_app_sid, token=twilio_api_token)
+
+    
+    def setNumMidiChannels(numChannels):
+        if not self.startedPlaying:
+            if numChannels <= 16:
+                self.availableChannels = range(1, numChannels + 1)
+            else:
+                print 'Error: limit is 16 channels'
+                return False
+        else:
+            print "Error, can't change number of midi channels in the middle of a session."
 
 
-	def stopNote(self, phone, digit):
-		if self.midiChannels.has_key(phone):
-			player = self.midiChannels[phone]
-		elif self.oldPlayers.has_key(phone):
-			player = self.oldPlayers[phone]
-		else:
-			player = None
-		
-		if player:
-			msg = player.getNoteOffMessage(digit)
-			if msg:
-				print "Sending midi message: channel={0} note={1}".format(msg.getChannel(), msg.getNoteNumber())
-				self.mout.sendMessage(msg)
+    def addPlayer(self, phone):
+        if not self.startedPlaying:
+            self.startedPlaying = True
+            self.midiFile = MIDIFile(10)
+            for i in range(0, 10):
+                self.midiFile.addTrackName(i, 0, str(i+1))
+                self.midiFile.addTempo(i, 0, 60)
+
+        if self.midiChannels.has_key(phone):
+            return True
+        
+        
+        channelCount = len(self.availableChannels)
+        if channelCount > 0:
+            self.midiChannels[phone] = PhoneMusician(phone, self.availableChannels.pop(random.randint(1, channelCount) - 1))
+            print "Adding player#" + phone
+            return True
+        
+        return False
+    
+    def removePlayer(self, phone):
+        if self.midiChannels.has_key(phone):
+            player = self.midiChannels.pop(phone)
+            channelNumber = player.midiChannel
+            print "Removing player {0} with midi channel {1}".format(phone, channelNumber)
+            self.availableChannels.append(channelNumber)
+            self.oldPlayers[phone] = player
+            if len(self.midiChannels) < 1:
+                fn = str(uuid.uuid1()) + '.mid'
+                f = open(fn, 'wb')
+                self.midiFile.writeFile(f)
+                f.close()
+                self.uploadMidiFile(fn)
+                
+    
+    def playNote(self, phone, digit):
+        if self.midiChannels.has_key(phone):
+            player = self.midiChannels[phone]
+            msg = player.getNoteOnMessage(digit)
+            if msg:
+                noteLength = 1.5
+                elapsedTime = (time.clock() - self.initialTime) * 60.0
+                self.midiFile.addNote(player.getMidiChannel() - 1, player.getMidiChannel(), msg.getNoteNumber(), elapsedTime, noteLength, 127)
+                print "Sending midi message: channel={0} note={1} vel={2}".format(msg.getChannel(), msg.getNoteNumber(), msg.getVelocity())
+                self.mout.sendMessage(msg)
+                t = threading.Timer(noteLength, self.stopNote, [phone, digit])
+                t.start()
+
+            return True
+
+
+    def stopNote(self, phone, digit):
+        if self.midiChannels.has_key(phone):
+            player = self.midiChannels[phone]
+        elif self.oldPlayers.has_key(phone):
+            player = self.oldPlayers[phone]
+        else:
+            player = None
+        
+        if player:
+            msg = player.getNoteOffMessage(digit)
+            if msg:
+                print "Sending midi message: channel={0} note={1}".format(msg.getChannel(), msg.getNoteNumber())
+                self.mout.sendMessage(msg)
--- midiphon_webserv.py
+++ midiphon_webserv.py
@@ -5,110 +5,112 @@ import tornado.ioloop
 from midiphon_midi import MidiManager
 from twilio import twiml
 
-myglob = 42
-
-midiManager = MidiManager()
+midiManager = MidiManager(16)
 
 def printget(handler):
-	print "Got a get in RecurseHandler!"
-	handler.write("""
-	Got request!<br /><br />
-	Path = '{0}'<br /><br />
-	Query = '{1}'<br /><br />
-	Headers = '{2}'<br /><br />
-	Arguments = '{3}'<br /><br />
-	Body = '{4}'<br /><br />
-	""".format(handler.request.path, handler.request.query, handler.request.headers, handler.request.arguments, handler.request.body))
+    '''Just prints out request info, easier to check if localtunnel's working.'''
+    print "Got a get in a " + handler.__class__.__name__
+    handler.write("""
+    Got request!<br /><br />
+    Path = '{0}'<br /><br />
+    Query = '{1}'<br /><br />
+    Headers = '{2}'<br /><br />
+    Arguments = '{3}'<br /><br />
+    Body = '{4}'<br /><br />
+    """.format(handler.request.path, handler.request.query, handler.request.headers, handler.request.arguments, handler.request.body))
 
-	print "Done with RecurseHandler."
+    print "Done with RecurseHandler" + handler.__class__.__name__
 
 
 class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        printget(self)
+    
+    def post(self):
+        global midiManager
+        r = twiml.Response()
+        r.say("Welcome to mideefawn.")
+        phoneNumber = self.request.arguments["From"][0]
+        if midiManager.addPlayer(phoneNumber) == False:
+            r.say("All channels full.")
+            r.hangup()
+        else:
+            r.say('Press a key to play a note')
+            r.gather(action='/recurse', numDigits=1)
 
-	def get(self):
-		print "Got a get in MainHandler!"
-		self.write("""
-		Got request!<br /><br />
-		Path = '{0}'<br /><br />
-		Query = '{1}'<br /><br />
-		Headers = '{2}'<br /><br />
-		Arguments = '{3}'<br /><br />
-		Body = '{4}'<br /><br />
-		""".format(self.request.path, self.request.query, self.request.headers, self.request.arguments, self.request.body))
-
-		print "Done with MainHandler."
-	
-	def post(self):
-		global midiManager
-		r = twiml.Response()
-		r.say("Welcome to mideefawn.")
-		phoneNumber = self.request.arguments["From"][0]
-		if midiManager.addPlayer(phoneNumber) == False:
-			r.say("All channels full.")
-			r.hangup()
-		else:
-			r.say('Press a key to play a note')
-			r.gather(action='/recurse', numDigits=1)
-
-		self.write(r.toxml())
+        self.write(r.toxml())
 
 
 
 class RecurseHandler(tornado.web.RequestHandler):
-	def post(self):
-		global midiManager
-		r = twiml.Response()
-		phoneNumber = self.request.arguments['From'][0]
-		digit = self.request.arguments['Digits'][0]
-		if midiManager.playNote(phoneNumber, digit) == False:
-			r.say("You are not a valid person.")
-			r.hangup()
-		else:
-			r.say('Okay')
-			r.gather(action='/recurse', numDigits=1)
+    def post(self):
+        global midiManager
+        r = twiml.Response()
+        phoneNumber = self.request.arguments['From'][0]
+        digit = self.request.arguments['Digits'][0]
+        if midiManager.playNote(phoneNumber, digit) == False:
+            r.say("You are not a valid person.")
+            r.hangup()
+        else:
+            r.say('Okay')
+            r.gather(action='/recurse', numDigits=1)
 
-		self.write(r.toxml())
+        self.write(r.toxml())
 
-	
-	def get(self):
-		print "Got a get in RecurseHandler!"
-		self.write("""
-		Got request!<br /><br />
-		Path = '{0}'<br /><br />
-		Query = '{1}'<br /><br />
-		Headers = '{2}'<br /><br />
-		Arguments = '{3}'<br /><br />
-		Body = '{4}'<br /><br />
-		""".format(self.request.path, self.request.query, self.request.headers, self.request.arguments, self.request.body))
-
-		print "Done with RecurseHandler."
+    
+    def get(self):
+        printget(self)
 
 
 class StatusHandler(tornado.web.RequestHandler):
-	def post(self):
-		global midiManager
-		callstatus = self.request.arguments['CallStatus'][0]
-		print "Got call status callback!"
-		if callstatus == 'completed':
-			print "Got end call callback!"
-			phoneNumber = self.request.arguments['From'][0]
-			midiManager.removePlayer(phoneNumber)
-		else:
-			print 'Got call status: ' + callstatus
-	
-	def get(self):
-		printget(self)
-
-
-
-
-
-application = tornado.web.Application([
-    (r"/entry", MainHandler),
-    (r"/recurse", RecurseHandler),
-    (r"/status", StatusHandler),
-])
+    def post(self):
+        global midiManager
+        callstatus = self.request.arguments['CallStatus'][0]
+        print "Got call status callback!"
+        if callstatus == 'completed':
+            print "Got end call callback!"
+            phoneNumber = self.request.arguments['From'][0]
+            midiManager.removePlayer(phoneNumber)
+        else:
+            print 'Got call status: ' + callstatus
+    
+    def get(self):
+        printget(self)
 
 if __name__ == "__main__":
-    application.listen(8080)
+    import sys
+    import optparse
+
+    parser = optparse.OptionParser(
+        description='The server software for running your own MidiPHON instance.',
+        epilog='''
+        Check out the README for basic instructions, or visit
+        midiphon.bartnett.com for detailed setup instructions.
+        '''
+    )
+
+    parser.add_option('-n', '--num-midi-channels', type='int', dest='numChannels')
+    parser.add_option('-p', '--port', type='int', default=8080)
+
+    (options, args) = parser.parse_args()
+
+    if len(args) > 0:
+        sys.stdout.write('Error, unrecognized arguments: ')
+        for a in args:
+            sys.stdout.write(a + ' ')
+        print ''
+        sys.exit(1)
+
+    if (options.numChannels):
+        midiManager.setNumChannels(options)
+    
+
+    application = tornado.web.Application([
+        (r"/entry", MainHandler),
+        (r"/recurse", RecurseHandler),
+        (r"/status", StatusHandler),
+    ])
+
+    application.listen(options.port)
+    print 'Listening on port {0}'.format(options.port)
     tornado.ioloop.IOLoop.instance().start()
